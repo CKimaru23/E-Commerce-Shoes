6766763a5186c5cc8828d4f4138dabb29182664a
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-01-03 14:52:09 +0300

    dark.css done

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,4 +1,34 @@
+// SCROLL BUTTON CODE
+let mybutton = document.getElementById("myBtn");
 
+let clickAudio = document.getElementById("myAudio");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function preLoader() {
+	let loading = document.getElementById("loading");
+	let main = document.getElementById("main");
+	loading.style.display = "none";
+	main.style.display = "block";
+}
+
+//Dark mode toggle
+let x = document.createElement("LINK");
+x.setAttribute("rel", "stylesheet");
+x.setAttribute("type", "text/css");
+x.setAttribute("href", "./Assets/css/dark.css");
+
+let mode = document.getElementById('mode');
+mode.addEventListener('click', () => {
+	if (mode.src.match("moon")) {
+		mode.src = "./Assets/img/sunrise.avif";
+		document.head.appendChild(x);
+	} else {
+		mode.src = "./Assets/img/moon.avif";
+		document.head.removeChild(x);
+	}
+});
 
 //function to go to top
 function scrollFunction() {
